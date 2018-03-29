b9526488d39162bef3e136a05fd4ecb6eb9daca0
Cyril COLINET <cyril.colinet@epitech.eu>
2018-03-29 20:55:08 +0200

    :tada: Add second derivative

--- calc.py
+++ calc.py
@@ -6,3 +6,31 @@
 ##
 
 import sys
+
+def calc_derivative(data):
+	default = 0.0
+	derive = []
+	tmp = 0
+	sub = 1
+	derive.append(0)
+	print("Derivative:", file=sys.stderr)
+	for i in range(1, len(data) - 1):
+		calc = data[i + 1][1] - data[i - 1][1]
+		calc /= (data[i + 1][0] - data[i - 1][0])
+		derive.append(calc)
+		print("volume: %g ml -> %.2f" %(data[i][0], derive[i]))
+		if default < derive[i]:
+			default = derive[i]
+			sub = data[i][0]
+			tmp = i
+	derive.append(0)
+	print("\nEquivalent point at %g ml\n" % sub)
+	return derive
+
+def calc_second_derivative(derive, data):
+	print("Second derivative")
+	for i in range(1, len(derive) - 3):
+		ph = (derive[i + 2] - derive[i])
+		ph /= (data[i + 2][0] - data[i + 1][0])
+		print("volume: %g ml -> %.2f" % (data[i + 1][0], ph))
+	print("\nSecond derivative estimated:")
