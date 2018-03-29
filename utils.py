cf466dc4864673ab41ef90c3921bad257e914863
Cyril COLINET <cyril.colinet@epitech.eu>
2018-03-29 15:38:19 +0200

    :tada: Add check if file exists

--- utils.py
+++ utils.py
@@ -5,18 +5,26 @@
 ## utilfs functions
 ##
 
+from os import path
 import csv
 import sys
 
 def my_help():
-	print("USAGE:\n\t./109titration <file.csv>\n")
+	print("USAGE:\n\t./109titration <file>\n")
 	print("DESCRIPTION:\n\t<file.csv>\tCSV file for valies list")
 	exit(0)
 
 def check_arguments():
 	length = len(sys.argv)
 	if length != 2:
-		print("Wrong arguments number.\nUsage: ./109titration <file.csv>", file=sys.stderr)
+		print("Wrong arguments number.\nUsage: ./109titration <file>", file=sys.stderr)
 		exit(84)
 	elif length == 2 and sys.argv[1] == "-h":
 		my_help()
+	elif length == 2 and ".csv" not in sys.argv[1]:
+		print("Invalid CSV file.\nUsage: ./109titration <file>", file=sys.stderr)
+		exit(84)
+	else:
+		if not path.isfile(sys.argv[1]):
+			print("Non existant file.\nUsage: ./109titration <file>", file=sys.stderr)
+			exit(84)
